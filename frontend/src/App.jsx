8f1356a8cf9c58627db45bd2300ab90ce57c4394
Aditya <z5419633@ad.unsw.edu.au>
2023-11-05 22:16:56 +1100

    Added register page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { Routes, Route, BrowserRouter as Router, Outlet } from 'react-router-dom'
+import Register from './Register';
 
 function App () {
+  const [token, setToken] = React.useState(null)
   return (
-    <>Let&apos;s go!</>
+    <>
+    <Outlet/>
+    <Router>
+      <Routes>
+        <Route path = "/" element={<>Home Page</>}/>
+        <Route path = "/register" element={<Register token = {token} setToken = {setToken}/>}/>
+
+      </Routes>
+    </Router>
+    </>
   );
 }
 
---
+++ frontend/src/Register.jsx
@@ -0,0 +1,98 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Unstable_Grid2';
+import Alert from '@mui/material/Alert';
+import Stack from '@mui/material/Stack';
+import { useNavigate } from 'react-router-dom';
+
+function Register (props) {
+  const [email, setEmail] = React.useState('');
+  const [name, setName] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [passwordCheck, setPasswordCheck] = React.useState('');
+  const [errorPage, setErrorPage] = React.useState(false);
+  const [errorMsg, setErrorMsg] = React.useState('')
+  const navigate = useNavigate();
+
+  const register = async () => {
+    if (password !== passwordCheck) {
+      setErrorMsg('Passwords do not Match. Please Try again.')
+      setErrorPage(true)
+      return
+    } else if (password.length < 6) {
+      setErrorMsg('Password must be atleast 6 characters. Please Try again.')
+      setErrorPage(true)
+      return
+    } else if (name.length <= 1) {
+      setErrorMsg('Invalid Name. Please Try again.')
+      setErrorPage(true)
+      return
+    }
+    const response = await fetch('http://localhost:5005/user/auth/register', {
+      method: 'POST',
+      body: JSON.stringify({
+        email, name, password
+      }),
+      headers: {
+        'Content-type': 'application/json',
+      }
+    });
+    const data = await response.json();
+    if (data.error) {
+      setErrorMsg(data.error)
+      setErrorPage(true)
+    } else if (data.token) {
+      console.log(data.token)
+      props.setToken(data.token)
+      navigate('/')
+    }
+  };
+
+  return (
+    <>
+      {errorPage === true
+        ? (
+        <>
+          <Stack sx={{ width: '100%' }} spacing={2}>
+          <Alert severity="error"> {errorMsg} </Alert>
+          </Stack>
+        </>
+          )
+        : <></>}
+      <Box
+        component="form"
+        noValidate
+        autoComplete="off"
+        sx={
+          {
+            '& > :not(style)': { m: 1, width: '25ch' },
+            flexGrow: 3,
+          }}
+          style = {{ width: '40%', backgroundColor: '#ffffff', margin: 'auto', marginTop: '10%', borderRadius: '25px' }}
+          >
+        <Grid container spacing={2} item xs = 'auto' style = {{ margin: 'auto' }} >
+          <h2 style={{ margin: 'auto', marginBottom: '5%' }}>Register</h2>
+          <Grid xs={12}>
+            <TextField id="outlined-basic" label="Name" variant="outlined" onChange={e => setName(e.target.value)} />
+          </Grid>
+          <Grid xs={12}>
+            <TextField id="outlined-basic" label="Email" variant="outlined" onChange={e => setEmail(e.target.value)}/>
+          </Grid>
+          <Grid xs={12}>
+            <TextField id="outlined-basic" label="Pasword" variant="outlined" onChange={e => setPassword(e.target.value)}/>
+          </Grid>
+          <Grid xs={12}>
+            <TextField id="outlined-basic" label="Confirm Password" variant="outlined" onChange={e => setPasswordCheck(e.target.value)}/>
+          </Grid>
+          <Grid xs={12}>
+            <Button variant="text" onClick={register} style = {{ textAlign: 'center' }}>Submit</Button>
+          </Grid>
+        </Grid>
+      </Box>
+    </>
+  );
+}
+
+export default Register;
